4d8be130b2ecef6d0834cb560e050d2c58219e56
Thorben Schröder <stillepost@gmail.com>
2013-04-03 05:39:35 +0200

    Bumps to 0.0.49

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# 0.0.49 / Unreleased
+# 0.0.49 / 2013-04-02
 
 * Adds authorization
 
--- lib/devcenter-backend/version.rb
+++ lib/devcenter-backend/version.rb
@@ -1,5 +1,5 @@
 module Devcenter
   module Backend
-    VERSION = "0.0.48"
+    VERSION = "0.0.49"
   end
 end
